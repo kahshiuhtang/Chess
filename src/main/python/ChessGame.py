46682b86770985f86d2c4946fa47740237092fee
Kah Shiuh Tang <kahshiuh.tang@gmail.com>
2022-12-26 13:22:32 -0500

    Added Movement Outside of Pawn

--- src/main/python/ChessGame.py
+++ src/main/python/ChessGame.py
@@ -1,30 +1,80 @@
-import ChessPieces
+from ChessPieces import check_bishop, check_rook, check_knight, check_queen, check_king, check_pawn, convert
+
+
+def validate_move(move):
+    if len(move) < 6 or len(move) > 7:
+        return False
+    if not (move[0] == 'w' or move[0] == 'b'):
+        return False
+    if not move[1] in ChessGame.PIECES:
+        return False
+    coords = convert(move)
+    for i in coords:
+        if i < 0 or i > 7:
+            return False
+    return True
+
+
 class ChessGame:
+    PIECES = {"P", "K", "N", "Q", "R", "B"}
+    CORNERS = {"h1": 0, "h8": 1, "a1": 2, "a8": 3}
+
     def __init__(self):
-        self.board = [["bR", "bN", "bB", "bQ", "bK", "bB", "bN", "bR"],
+        self.board = [["bR", "bN", "bB", "bQ", "bK", "--", "--", "bR"],
                       ["bP", "bP", "bP", "bP", "bP", "bP", "bP", "bP"],
-                      ["  ", "  ", "  ", "  ", "  ", "  ", "  ", "  "],
-                      ["  ", "  ", "  ", "  ", "  ", "  ", "  ", "  "],
-                      ["  ", "  ", "  ", "  ", "  ", "  ", "  ", "  "],
-                      ["  ", "  ", "  ", "  ", "  ", "  ", "  ", "  "],
+                      ["--", "--", "--", "--", "--", "--", "--", "--"],
+                      ["--", "--", "--", "--", "--", "--", "--", "--"],
+                      ["--", "--", "--", "--", "--", "--", "--", "--"],
+                      ["--", "--", "--", "--", "--", "--", "--", "--"],
                       ["wP", "wP", "wP", "wP", "wP", "wP", "wP", "wP"],
-                      ["wR", "wN", "wB", "wQ", "wK", "wB", "wN", "wR"]]
+                      ["wR", "--", "--", "--", "wK", "--", "--", "wR"]]
+        # ["wR", "wN", "wB", "wQ", "wK", "--", "wN", "wR"]
+        # ["bR", "bN", "bB", "bQ", "bK", "bB", "bN", "bR"]
+        self.moved = [False, False, False, False, False, False] #uL -> uR -> bL -> bR -> wK -> bK of rooks
+        self.turn = "w"
+        self.movedPawnsB = [False, False, False, False, False, False, False, False]
+        self.movedPawnsW = [False, False, False, False, False, False, False, False]
+        self.moves = []
 
     def move_piece(self, move):
+        if not validate_move(move):
+            return False
+        if not self.turn == move[0]:
+            return False
         pieceType = move[1]
         if pieceType == "R":
-            return checkRook(move, self.board)
+            return check_rook(move, self.board)
         elif pieceType == "N":
-            return checkKnight(move, self.board)
+            return check_knight(move, self.board)
         elif pieceType == "B":
-            return checkBishop(move, self.board)
+            return check_bishop(move, self.board)
         elif pieceType == "Q":
-            return checkQueen(move, self.board)
+            return check_queen(move, self.board)
         elif pieceType == "K":
-            return checkKing(move, self.board)
+            return check_king(move, self.board, self.moved)
         elif pieceType == "P":
-            return checkPawn(move, self.board)
-        return false
+            return check_pawn(move, self.board, self.moves)
+        return False
+
+    def move(self, move):
+        if self.move_piece(move):
+            coords = convert(move) #Make sure to mark that king or rook has moved
+            self.board[coords[2]][coords[3]] = move[0:2]
+            self.board[coords[0]][coords[1]] = "--"
+            row = 7 if move[0] == "w" else 0
+            if move[1] == "R" and move[2:4] in ChessGame.CORNERS.keys():
+                self.moved[int(ChessGame.CORNERS[move[2:4]])] = True
+                print(self.moved)
+            if move[1] == "K" and abs(coords[1] - coords[3]) == 2 and abs(coords[0] - coords[2]) == 0 and move[4] == "g":
+                self.board[row][5] = move[0]+"R"
+                self.board[row][7] = "--"
+            elif move[1] == "K" and abs(coords[1] - coords[3]) == 2 and abs(coords[0] - coords[2]) == 0:
+                self.board[row][3] = move[0]+"R"
+                self.board[row][0] = "--"
+            self.turn = "w" if self.turn == "b" else "b"
+            self.moves.append(move)
+            return True
+        return False
 
     def print_board(self):
         for i in range(8):
@@ -32,13 +82,15 @@ class ChessGame:
                 print(self.board[i][j], end=" ")
             print()
 
-
-def convert_l(symbol):
-    return ord(symbol) - 97
-
-
-def convert_n(num):
-    return 8-num
+    def print_moves(self):
+        for move in self.moves:
+            print(move)
 
 
 c = ChessGame()
+c.move("wRa8a7")
+c.move("bPe7e5")
+c.move("wKe1g1")
+c.move("bKe8g8")
+c.print_board()
+c.print_moves()
--- src/main/python/ChessPieces.py
+++ src/main/python/ChessPieces.py
@@ -1,18 +1,96 @@
-def checkBishop(move,game):
-    return false
+
+def check_bishop(move,game):
+    coords = convert(move)
+    if not abs(coords[0]-coords[2]) == abs(coords[1]-coords[3]):
+        return False
+    xInc = 1 if coords[2]-coords[0] > 0 else -1
+    yInc = 1 if coords[3] - coords[1] > 0 else -1
+    return bishop_help(coords, game, xInc, yInc, move[0])
 
 
-def checkPawn(move, game):
-    return false
+def bishop_help(coords, game, x_inc, y_inc, is_white):
+    for i in range(abs(coords[0]-coords[2])-1):
+        coords[0] += x_inc
+        coords[1] += y_inc
+        if not game[coords[0]][coords[1]] == "--":
+            return False
+    return game[coords[2]][coords[3]] == "--" or not game[coords[2]][coords[3]][0] == is_white
 
-def checkRook(move, game):
-    return false
 
-def checkQueen(move, game):
-    return false
+def check_pawn(move, game, moves):
+    return True
 
-def checkKing(move, game):
-    return false
 
-def checkKnight(move, game):
-    return false
+def check_rook(move, game):
+    coords = convert(move)
+    if not abs(coords[0] - coords[2]) == 0 and not abs(coords[1] - coords[3]) == 0:
+        return False
+    xInc = 0 if abs(coords[0] - coords[2]) == 0 else 1 if coords[2] - coords[0] > 0 else -1
+    yInc = 0 if abs(coords[3] - coords[1]) == 0 else 1 if coords[3] - coords[1] > 0 else -1
+    return rook_help(coords, game, xInc, yInc, move[0])
+
+
+def rook_help(coords, game, x_inc, y_inc, is_white):
+    for i in range(max(abs(coords[0]-coords[2]),abs(coords[3]-coords[1]))-1):
+        coords[0] += x_inc
+        coords[1] += y_inc
+        if not game[coords[0]][coords[1]] == "--":
+            return False
+    return game[coords[2]][coords[3]] == "--" or not game[coords[2]][coords[3]][0] == is_white
+
+
+def check_queen(move, game):
+    return check_bishop(move, game) or check_rook(move, game)
+
+def check_king(move, game, rooks):
+    coords = convert(move)
+    xDif = abs(coords[0] - coords[2])
+    yDif = abs(coords[1] - coords[3])
+    is_white = "b" if move[0] == "b" else "w"
+    if yDif == 2 and xDif == 0:
+        return check_castle(coords, game, is_white, rooks, move)
+    elif yDif == 1 and xDif == 1:
+        return game[coords[2]][coords[3]] == "--" or not is_white == game[coords[2]][coords[3]][0]
+    return False
+
+
+def check_castle(coords, game, is_white, rooks, move):
+    row = 0 if is_white == "b" else 2
+    squaresToCheck = 3 if coords[3] - coords[1] < 0 else 2
+    inc = -1 if squaresToCheck == 3 else 1
+    for i in range(squaresToCheck):
+        coords[1] += inc
+        if not game[coords[0]][coords[1]] == "--":
+            return False
+    add = 0 if squaresToCheck == 3 else 1
+    row += add
+    return not rooks[row]
+
+
+
+def check_knight(move, game):
+    coords = convert(move)
+    xDif = abs(coords[0]-coords[2])
+    yDif = abs(coords[1]-coords[3])
+    if not (xDif == 2 and yDif == 1) or (xDif == 1 and yDif == 2):
+        return False
+    is_white = move[0]
+    return game[coords[2]][coords[3]] == "--" or not game[coords[2]][coords[3]][0] == is_white
+
+
+def convert_s(symbol):
+    return (ord(symbol) - 97)
+
+
+def convert_n(num):
+    return 8-int(num)
+
+
+def convert(move):
+    x1 = int(convert_n(move[3]))
+    y1 = int(convert_s(move[2]))
+    x2 = int(convert_n(move[5]))
+    y2 = int(convert_s(move[4]))
+    return [x1, y1, x2, y2]
+
+
