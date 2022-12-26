4381ef694f0d72278137086df3a85036deb2545f
Kah Shiuh Tang <kahshiuh.tang@gmail.com>
2022-12-26 14:32:53 -0500

    Added En Passant

--- src/main/python/ChessGame.py
+++ src/main/python/ChessGame.py
@@ -18,22 +18,23 @@ def validate_move(move):
 class ChessGame:
     PIECES = {"P", "K", "N", "Q", "R", "B"}
     CORNERS = {"h1": 0, "h8": 1, "a1": 2, "a8": 3}
+    KINGS = {"e1": 4, "e8": 5}
 
     def __init__(self):
         self.board = [["bR", "bN", "bB", "bQ", "bK", "--", "--", "bR"],
                       ["bP", "bP", "bP", "bP", "bP", "bP", "bP", "bP"],
                       ["--", "--", "--", "--", "--", "--", "--", "--"],
                       ["--", "--", "--", "--", "--", "--", "--", "--"],
-                      ["--", "--", "--", "--", "--", "--", "--", "--"],
+                      ["--", "--", "--", "--", "--", "bP", "--", "--"],
                       ["--", "--", "--", "--", "--", "--", "--", "--"],
                       ["wP", "wP", "wP", "wP", "wP", "wP", "wP", "wP"],
-                      ["wR", "--", "--", "--", "wK", "--", "--", "wR"]]
+                      ["wR", "--", "--", "--", "wK", "--", "bP", "wR"]]
         # ["wR", "wN", "wB", "wQ", "wK", "--", "wN", "wR"]
         # ["bR", "bN", "bB", "bQ", "bK", "bB", "bN", "bR"]
         self.moved = [False, False, False, False, False, False] #uL -> uR -> bL -> bR -> wK -> bK of rooks
         self.turn = "w"
-        self.movedPawnsB = [False, False, False, False, False, False, False, False]
-        self.movedPawnsW = [False, False, False, False, False, False, False, False]
+        self.movedPawns = [[False, False, False, False, False, False, False, False],
+                           [False, False, False, False, False, False, False, False]]
         self.moves = []
 
     def move_piece(self, move):
@@ -53,7 +54,7 @@ class ChessGame:
         elif pieceType == "K":
             return check_king(move, self.board, self.moved)
         elif pieceType == "P":
-            return check_pawn(move, self.board, self.moves)
+            return check_pawn(move, self.board, self.moves, self.movedPawns)
         return False
 
     def move(self, move):
@@ -62,9 +63,16 @@ class ChessGame:
             self.board[coords[2]][coords[3]] = move[0:2]
             self.board[coords[0]][coords[1]] = "--"
             row = 7 if move[0] == "w" else 0
+            if move[1] == "K" and move[2:4] in ChessGame.KINGS.keys():
+                self.moved[int(ChessGame.CORNERS[move[2:4]])] = True
             if move[1] == "R" and move[2:4] in ChessGame.CORNERS.keys():
                 self.moved[int(ChessGame.CORNERS[move[2:4]])] = True
-                print(self.moved)
+            if move[1] == "P" and (move[3] == "7" or move[3] == "2"):
+                row = 0 if move[0] == "w" else 1
+                self.movedPawns[row][coords[1]] = True
+            if move[1] == "P" and not move[2] == move[4]:
+                inc = 1 if move[0] == "w" else -1
+                self.board[coords[2]+inc][coords[3]] = "--"
             if move[1] == "K" and abs(coords[1] - coords[3]) == 2 and abs(coords[0] - coords[2]) == 0 and move[4] == "g":
                 self.board[row][5] = move[0]+"R"
                 self.board[row][7] = "--"
@@ -84,13 +92,16 @@ class ChessGame:
 
     def print_moves(self):
         for move in self.moves:
-            print(move)
+            print(move, end="->")
+        print("END")
 
 
 c = ChessGame()
-c.move("wRa8a7")
-c.move("bPe7e5")
-c.move("wKe1g1")
-c.move("bKe8g8")
+#c.move("wRh1g1")
+#c.move("bPe7e5")
+#c.move("wKe1g1")
+#c.move("bKe8g8")
+c.move("wPe2e4")
+c.move("bPf4e3")
 c.print_board()
 c.print_moves()
--- src/main/python/ChessPieces.py
+++ src/main/python/ChessPieces.py
@@ -17,8 +17,26 @@ def bishop_help(coords, game, x_inc, y_inc, is_white):
     return game[coords[2]][coords[3]] == "--" or not game[coords[2]][coords[3]][0] == is_white
 
 
-def check_pawn(move, game, moves):
-    return True
+def check_pawn(move, game, moves, pawns):
+    coords = convert(move)
+    xDif = abs(coords[0] - coords[2])
+    yDif = abs(coords[1] - coords[3])
+    is_white = -1 if move[0] == "w" else 1
+    if yDif == 0 and xDif == 1:
+        return game[coords[2]][coords[3]] == "--" and is_white == coords[2] - coords[0]
+    elif yDif == 0 and xDif == 2 and is_white*2 == coords[2] - coords[0]:
+        row = 0 if move[0] == "w" else 1
+        inc = -1 if move[0] == "w" else 1
+        return (not pawns[row][coords[1]]) and game[coords[0]+inc][coords[1]] == "--" and game[coords[0]+inc*2][coords[1]] == "--" and (move[3] == "7" or move[3] == "2")
+    elif yDif == 1 and xDif == 1 and is_white == coords[2] - coords[0]:
+        if game[coords[2]][coords[3]] == "--":
+            prevMove = moves[len(moves)-1]
+            coords2 = convert(prevMove)
+            return abs(coords2[2] - coords2[0]) == 2 and coords2[3] == coords[3] and coords2[2]+is_white == coords[2]
+        else:
+            return not game[coords[2]][coords[3]][0] == move[0]
+         # EnPassant, Normal, Check if Moving in Right Direction
+    return False
 
 
 def check_rook(move, game):
@@ -41,6 +59,7 @@ def rook_help(coords, game, x_inc, y_inc, is_white):
 
 def check_queen(move, game):
     return check_bishop(move, game) or check_rook(move, game)
+
 
 def check_king(move, game, rooks):
     coords = convert(move)
